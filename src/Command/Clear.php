61ce375b296b9fcf81ce3b03db9948fa8351a7bf
RunnerLee <runnerleer@gmail.com>
2018-02-09 14:51:23 +0800

    init

---
+++ src/Command/Clear.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @author: RunnerLee
+ * @email: runnerleer@gmail.com
+ * @time: 2018-02
+ */
+
+namespace Runner\Pocache\Command;
+
+use FastD\Http\Request;
+use FastD\Http\Response;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class Clear extends Command
+{
+
+    public function configure()
+    {
+        $this->setName('opcache:clear');
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        $request = new Request('POST', config()->get('app_url') . '/flush_opcache');
+
+        $response = $request->send([
+            'token' => config()->get('opcache.flush_token')
+        ]);
+
+        if (Response::HTTP_OK === $response->getStatusCode()) {
+            $output->writeln('<info>opcode cache cleared.</info>');
+        } else {
+            $output->writeln('<error>Clear opcode cache failed.</error>');
+        }
+    }
+}
---
+++ src/FastdPocacheServiceProvider.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * @author: RunnerLee
+ * @email: runnerleer@gmail.com
+ * @time: 2018-02
+ */
+
+namespace Runner\Pocache;
+
+use FastD\Container\Container;
+use FastD\Container\ServiceProviderInterface;
+use FastD\Http\Response;
+use FastD\Http\ServerRequest;
+use Runner\Pocache\Command\Clear;
+
+class FastdPocacheServiceProvider implements ServiceProviderInterface
+{
+
+    /**
+     * @param Container $container
+     * @return mixed
+     */
+    public function register(Container $container)
+    {
+        route()->post('/flush_opcache', function (ServerRequest $request) {
+            if (
+                $request->getParam('token', '') !== config()->get('opcache.flush_token')
+                || !function_exists('opcache_reset')
+            ) {
+                return new Response('', Response::HTTP_NO_CONTENT);
+            }
+
+            opcache_reset();
+
+            return new Response('ok', Response::HTTP_OK);
+        });
+
+        config()->merge([
+            'consoles' => [
+                Clear::class,
+            ],
+        ]);
+    }
+}
